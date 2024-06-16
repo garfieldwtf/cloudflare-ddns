780434e07421e2e3f1fa15c238df2bbffe0f7954
root <admin@garfield.in>
2024-06-17 05:51:30 +0800

    update files

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM alpine:latest
 
 # Install necessary packages: tailscale, openrc, and bash in a single layer
 RUN apk update &&\
-    apk add --no-cache cloudflare-ddns bash 
+    apk add --no-cache cloudflare-ddns bash dcron
 
 # Copy the entrypoint script and set permissions in one layer
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,16 @@
 /usr/bin/cloudflare-ddns ${cf-email} ${cf-global-api} ${cf-domain}
 
 # Create a cron job to run the command every 5 minutes
-echo "*/5 * * * * /usr/bin/cloudflare-ddns ${cf-email} ${cf-global-api} ${cf-domain} >> /var/log/cloudflare-ddns.log 2>&1" > /etc/cron.d/cloudflare-ddns
+echo "*/5 * * * * /usr/bin/cloudflare-ddns ${cf-email} ${cf-global-api} ${cf-domain} >> /var/log/cloudflare-ddns.log 2>&1" >> /etc/crontabs/root
 
 # Give execution rights on the cron job
-chmod 0644 /etc/cron.d/cloudflare-ddns
-
-# Apply cron job
-crontab /etc/cron.d/cloudflare-ddns
+chmod +x /etc/crontabs/root
 
 # Create the log file to be able to run tail
 touch /var/log/cloudflare-ddns.log
 
 # Start cron
-cron
+crond
 
 # Keep the container running
 tail -f /var/log/cloudflare-ddns.log
