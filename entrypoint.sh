addc108abb551a5410bfd94f76a5219266db837f
root <admin@garfield.in>
2024-06-17 06:12:15 +0800

    update files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run the cloudflare-ddns command once when the container starts
-/usr/bin/cloudflare-ddns ${cf-email} ${cf-global-api} ${cf-domain}
+/usr/bin/cloudflare-ddns ${CF_EMAIL} ${CF_GLOBAL_API} ${CF_DOMAIN}
 
 # Create a cron job to run the command every 5 minutes
-echo "*/5 * * * * /usr/bin/cloudflare-ddns ${cf-email} ${cf-global-api} ${cf-domain} >> /var/log/cloudflare-ddns.log 2>&1" >> /etc/crontabs/root
+echo "*/5 * * * * /usr/bin/cloudflare-ddns ${CF_EMAIL} ${CF_GLOBAL_API} ${CF_DOMAIN} >> /var/log/cloudflare-ddns.log 2>&1" >> /etc/crontabs/root
 
 # Give execution rights on the cron job
 chmod +x /etc/crontabs/root
